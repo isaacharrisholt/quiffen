c45ce543aa25677068b21f12be354937d2af0542
Isaac Harris-Holt <isaac@harris-holt.com>
2021-06-25 14:38:05 +0100

    Updated imports

--- quiffen/__init__.py
+++ quiffen/__init__.py
@@ -1,2 +1,4 @@
-from quiffen.core.qif_parser import QifParser
-from quiffen.core.transactions import Transaction, TransactionList
+from quiffen.core.qif import Qif
+from quiffen.core.transactions import Transaction, Investment, TransactionList
+from quiffen.core.accounts import Account
+from quiffen.core.categories_classes import Category, Class
