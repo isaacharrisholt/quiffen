10caa10b2371b2932d168b3bf316c0b3c3761797
Isaac Harris-Holt <isaac@harris-holt.com>
2022-01-27 11:40:57 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,29 @@
-#!/usr/bin/env python
+from setuptools import setup, find_packages
 
-from setuptools import setup
+VERSION = '1.2.1'
+DESCRIPTION = 'Quiffen'
+with open('README.rst', 'r') as f:
+    LONG_DESCRIPTION = f.read()
 
+# Setting up
 setup(
-    name='quiffen',
-    version='1.0',
-    description='Quiffen is a Python package for parsing QIF (Quicken Interchange Format) files.',
-    author='Isaac Harris-Holt',
-    author_email='isaac@harris-holt.com',
-    url='https://github.com/isaacharrisholt/quiffen',
-    packages=['quiffen', 'quiffen.core'],
+    name="quiffen",
+    version=VERSION,
+    author="Isaac Harris-Holt",
+    author_email="isaac@harris-holt.com",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/x-rst',
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['qif', 'finance', 'data processing'],
+    license='GNU GPLv3',
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows"
+    ],
+    python_requires='>=3'
 )
